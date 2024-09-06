bb3e981723eaa4aa1780c469f17b900c08cbc0de
marc condon <marc@fogtest.com>
2024-09-06 10:16:34 -0700

    not-found pages checkin before parallel paths

---
+++ app/news/[slug]/not-found.js
@@ -0,0 +1,9 @@
+export default function NewsNotFoundFound()
+{
+    return (
+        <div id="error">
+            <h1>"Not Found"</h1>
+            <p>Unfortunately, we could find the requested article.</p>
+        </div>
+    )
+}
--- app/news/page.js
+++ app/news/page.js
@@ -1,6 +1,5 @@
-import classes from './page.module.css';
 import Link from "next/link";
-
+import {DUMMY_NEWS} from "@/dummy-news";
 
 export default async function NewsPage() {
 
@@ -11,15 +10,15 @@ export default async function NewsPage() {
                     News Page
                 </h1>
                 <ul className="news-list">
-                    <li>
-                        <Link href="/news/first-news">First News</Link>
-                    </li>
-                    <li>
-                        <Link href="/news/second-news">Second News</Link>
-                    </li>
-                    <li>
-                        <Link href="/news/third-news">Third News</Link>
-                    </li>
+                    {DUMMY_NEWS.map(newsItem => (
+                        <li key={newsItem.id}>
+                            <Link href={`/news/${newsItem.slug}`} passHref>
+                                <img src={`/images/news/${newsItem.image}`} alt={newsItem.title}/>
+                                <span>{newsItem.title}</span>
+                            </Link>
+                        </li>
+                    ))}
+
                 </ul>
             </main>
         </>
