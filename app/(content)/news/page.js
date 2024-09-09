b195e5e3542749c7f9f31dd5124e2a8476ffa088
marc condon <marc@fogtest.com>
2024-09-09 12:15:02 -0700

    checking in client-side data fetching for reference before switching to server-side data fetching

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,13 +1,44 @@
-import {DUMMY_NEWS} from "@/dummy-news";
-import NewsList from "@/components/news-list";
+'use client'
 
-export default async function NewsPage() {
+import NewsList from "@/components/news-list";
+import {useEffect, useState} from "react";
+
+export default function NewsPage() {
+    const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const [news, setNews] = useState();
+
+    useEffect(() => {
+        async function fetchNews() {
+            setIsLoading(true);
+            const response = await fetch('http://localhost:8080/news', {})
+            if (!response.ok) {
+                setError('Failed to fetch news.')
+                setIsLoading(false);
+            }
+            const news = await response.json();
+            setIsLoading(false);
+            setNews(news);
+        }
+        fetchNews()
+    }, [])
+    if (isLoading){
+        return <p>Loading...</p>;
+    }
+    if (error){
+        return <p>{error}</p>;
+    }
+    let newsContent
+
+    if (news){
+        newsContent = <NewsList news={news} />;
+    }
     return (
         <>
             <h1>
                 News Page
             </h1>
-            <NewsList news={DUMMY_NEWS}/>
+            {newsContent}
         </>
     );
 }
