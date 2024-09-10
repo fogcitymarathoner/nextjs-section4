44e950737284ed2c5b63f2a43f244f8b35da4954
marc condon <marc@fogtest.com>
2024-09-09 18:21:32 -0700

    checking in "migrating data to local source" Moved back button in InterceptedImagePage to a client component to use async/await

--- app/(content)/news/[slug]/image/page.js
+++ app/(content)/news/[slug]/image/page.js
@@ -1,23 +1,19 @@
-import {DUMMY_NEWS} from "@/dummy-news";
 import {notFound} from "next/navigation";
+import {getNewsItem} from "@/lib/news";
 
-export default function ImagePage({params}) {
+export default async function ImagePage({params}) {
     const newsItemSlug = params.slug
-
-    const newsItem = DUMMY_NEWS.find(newsItem => newsItem.slug === newsItemSlug)
+    const newsItem = await getNewsItem(newsItemSlug)
     if (!newsItem) {
         notFound()
     }
-
     return (
         <>
-
-            {/*             <div className="modal-backdrop"/>
-            <dialog className="modal" open>*/}
-            <div className="fullscreen-image">
-                <img src={`/images/news/${newsItem.image}`} alt={newsItem.title}/>
-            </div>
-            {/*</dialog>*/}
+            <dialog className="modal" open>
+                <div className="fullscreen-image">
+                    <img src={`/images/news/${newsItem.image}`} alt={newsItem.title}/>
+                </div>
+            </dialog>
         </>
     )
 }
--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,44 +1,14 @@
-'use client'
 
 import NewsList from "@/components/news-list";
-import {useEffect, useState} from "react";
+import {getAllNews} from "@/lib/news";
 
-export default function NewsPage() {
-    const [error, setError] = useState(null);
-    const [isLoading, setIsLoading] = useState(false);
-    const [news, setNews] = useState();
+export default async function NewsPage() {
+    const news = await getAllNews();
 
-    useEffect(() => {
-        async function fetchNews() {
-            setIsLoading(true);
-            const response = await fetch('http://localhost:8080/news', {})
-            if (!response.ok) {
-                setError('Failed to fetch news.')
-                setIsLoading(false);
-            }
-            const news = await response.json();
-            setIsLoading(false);
-            setNews(news);
-        }
-        fetchNews()
-    }, [])
-    if (isLoading){
-        return <p>Loading...</p>;
-    }
-    if (error){
-        return <p>{error}</p>;
-    }
-    let newsContent
-
-    if (news){
-        newsContent = <NewsList news={news} />;
-    }
     return (
         <>
-            <h1>
-                News Page
-            </h1>
-            {newsContent}
+            <h1>News Page</h1>
+            <NewsList news={news} />
         </>
     );
 }
